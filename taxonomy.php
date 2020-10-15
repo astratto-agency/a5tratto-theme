f07a47c3ff5e33a066068402b604ea6d3a2c7b22
A5TRATTO TEAM <a5trattoteam@gmail.com>
2020-10-16 01:19:13 +0200

    Theme Update progetto artewsc

--- taxonomy.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-Template Name: Template Taxonomy
-*/
-?>
-<?php get_part('templates/taxonomy'); ?>
--- template-home.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Template Name: Template Home 
-*/
-?>
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_part('templates/content-home'); ?>
-<?php endwhile; ?>
