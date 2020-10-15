610dc2d9d166d859607cdc11f4f0701c826b3d25
A5TRATTO TEAM <a5trattoteam@gmail.com>
2020-10-16 01:06:20 +0200

    Theme Update progetto arte

--- single.php
+++
@@ -1,3 +0,0 @@
-<?php while (have_posts()) : the_post(); ?>
-	<?php get_part('templates/single'); ?>
-<?php endwhile; ?>
--- tag.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Template Name: Template Tag
-*/
-?>
-<?php get_part('templates/tag'); ?>
-<!-- se la categoria la vuoi vuoi spcifica  -->
-<?php /*get_part('templates/category-'.$category_name); */?>
