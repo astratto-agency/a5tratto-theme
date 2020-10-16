19f0f58ec179614c625159da5c5bfbf4fbdf53f8
A5TRATTO TEAM <a5trattoteam@gmail.com>
2020-10-16 23:16:57 +0200

    o7.10

--- archive.php
+++
@@ -1,8 +0,0 @@
-<!-- A_SETTINGS Impostazione pagina Themplate Archive -->
-<?php
-/*
-Template Name: Template Archive
-*/
-?>
-<?php get_part('templates/archive'); ?>
-
--- searchform.php
+++
@@ -1,9 +0,0 @@
-<!-- A_SETTINGS Impostazione pagina Template Search Form -->
-<?php
-/*
-Template Name: Template Search From
-*/
-?>
-<?php get_part('templates/searchform'); ?>
-
-
