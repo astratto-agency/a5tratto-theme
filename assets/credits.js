72401040f3972dc465f12b151dd425d0d069f875
A5TRATTO TEAM <a5trattoteam@gmail.com>
2020-10-17 16:44:47 +0200

    A5tratto-Theme-Origin-Master-73.52

---
+++ assets/credits.js
@@ -0,0 +1,4 @@
+﻿// Credits
+console.log('© Azienda segnaposto di Tizio, Caio & Co.. Tutti i diritti sono riservati');
+console.log('Realizzazione siti web %cA5TRATTO srl', 'color: #3ABEB9; font-weight:600;');
+console.log('https://www.a5tratto.it');
---
+++ page.php
@@ -0,0 +1,3 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
+
+ <?php get_part('templates/content'); ?>
