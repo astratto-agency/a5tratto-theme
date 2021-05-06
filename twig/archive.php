6659670075f5d552c3ae58d39bdd15043490fcea
A5TRATTO TEAM <a5trattoteam@gmail.com>
2021-05-06 14:35:06 +0200

    V.7.3.5.0 update & fix routing archive.php

--- twig/archive.php
+++ twig/archive.php
@@ -15,24 +15,24 @@
  */
 
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 $post = Timber::query_post();
 $context = Timber::get_context();
 
 // A_SETTINGS Assegnazione dei template
-$templates = array( 'archive.twig', 'index.twig' );
+$templates = array('archive.twig', 'index.twig');
 
 // A_SETTINGS Assegnazione del numero di paginazione di post per pagina
 $paginazione = 4;
 
 // A_SETTINGS Elaborazione dell'impaginato impostare il numero successivo qui '%/page/([0-3]+)%' in base al valore assegnato nella paginazione
-preg_match('%/page/([0-3]+)%', $_SERVER['REQUEST_URI'], $matches );
-if ( get_query_var( 'paged' ) ) {
-    $paged = get_query_var( 'paged' );
-} elseif ( get_query_var( 'page' ) ) {
-    $paged = get_query_var( 'page' );
+preg_match('%/page/([0-3]+)%', $_SERVER['REQUEST_URI'], $matches);
+if (get_query_var('paged')) {
+    $paged = get_query_var('paged');
+} elseif (get_query_var('page')) {
+    $paged = get_query_var('page');
 } else {
-    $paged = isset( $matches[1] ) ? $matches[1] : 1;
+    $paged = isset($matches[1]) ? $matches[1] : 1;
 }
 if (!isset($paged) || !$paged) {
     $paged = 1;
@@ -43,74 +43,81 @@ $context['term'] = $term = new Timber\Term(get_queried_object_id());
 
 // A_SETTINGS Smistamento delle impaginazioni ai relativi template di pagina
 $context['title'] = 'Archive';
-if ( is_day() ) {
+if (is_day()) {
     // A_SETTINGS day
     $context['title'] = 'Archive: ' . get_the_date('D M Y'); // Update title
-} else if ( is_month() ) {
+} else if (is_month()) {
     // A_SETTINGS month
     $context['title'] = 'Archive: ' . get_the_date('M Y'); // Update title
-} else if ( is_year() ) {
+} else if (is_year()) {
     // A_SETTINGS year
     $context['title'] = 'Archive: ' . get_the_date('Y'); // Update title
-} else if ( is_tag() ) {
+} else if (is_tag()) {
     // A_SETTINGS tag
     $context['title'] = single_tag_title('', false); // Update title
     array_unshift($templates, 'tag-' . $term->slug . '.twig', 'tag.twig'); // Update templates
-} else if ( is_category() ) {
+} else if (is_category()) {
     // A_SETTINGS category
     $context['title'] = single_cat_title('Title Category', false); // Update title
     array_unshift($templates, 'category-' . $term->slug . '.twig', 'category.twig');// Update templates
-} else if ( is_tax() ) {
+} else if (is_tax()) {
     // A_SETTINGS taxonomy
     $context['title'] = single_cat_title('', false); // Update title
     array_unshift($templates, 'taxonomy-' . $term->slug . '.twig', 'taxonomy.twig'); // Update templates
-} else if ( is_post_type_archive() ) {
+} else if (is_post_type_archive()) {
     // A_SETTINGS archive
     $context['title'] = post_type_archive_title('', false); // Update title
     array_unshift($templates, 'archive-' . get_post_type() . '.twig'); // Update templates
 }
 
+
 //  A_SETTINGS Assegno tutte le variabili di ACF a Twig
 // inSorry, we couldn't find what you're looking fo caso avessi necessità puoi sostituire il valore $post con l'ID della pagina
-$fields = get_field_objects( $post->id );
-if( $fields ):
-    foreach( $fields as $field ):
+$fields = get_field_objects($post->id);
+if ($fields):
+    foreach ($fields as $field):
         $name_id = $field['name'];
         $value_id = $field['value'];
         $context[$name_id] = $value_id;
     endforeach;
 endif;
 
-
-//  A_SETTINGS Check if is page and set standard post_type
-if (get_post_type() == 'page' ) {
-    $post_type == 'post';
-} else {
-    $post_type == get_post_type();
+//  A_SETTINGS Controllo se la pagina è un CPT con slug simile
+$cpt_args = array(
+    'public' => true,
+    '_builtin' => false
+);
+$post_types = get_post_types($cpt_args, 'object'); // use 'names' if you want to get only name of the post type.
+$post_type = 'post';
+foreach ($post_types as $item_post_type) {
+    if ($post->slug == $item_post_type->rewrite['slug'] || $post->slug == $item_post_type->name) {
+        $post_type = $item_post_type->name;
+        $context['title'] = $item_post_type->label;
+    }
 }
-$obj_post_type = get_post_type_object( $post_type );
+$obj_post_type = get_post_type_object($post_type);
+
+
 
 //  A_SETTINGS QUERY elaboro query base
-$args= array(
+$args = array(
     'post_type' => $post_type,
     'posts_per_page' => $paginazione,
-    'paged'                 => $paged,  //////////// Impaginazione
+    'paged' => $paged,  //////////// Impaginazione
     'orderby' => 'date',
     'order' => 'ASC',
 );
 $context['posts'] = $query_posts = new Timber\PostQuery($args);
 
 
-
-
 //  A_SETTINGS Gestisco la numerazione della pagine e i corrispettivi valori trovati
 $context['found_posts'] = $query_posts->found_posts;
 $context['startpost'] = $startpost = 1;
-$context['startpost'] = $startpost =  $paginazione*($paged - 1)+1;
+$context['startpost'] = $startpost = $paginazione * ($paged - 1) + 1;
 $context['endpost'] = $endpost = ($paginazione * $paged < $query_posts->found_posts ? $paginazione * $paged : $query_posts->found_posts);
 
 //  A_SETTINGS Elaboro template a Twig
-Timber::render( $templates, $context );
+Timber::render($templates, $context);
 
 /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :::::::::::::: 11 * A_ISTRUCTIONS
