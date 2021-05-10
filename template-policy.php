3d5b59fefb4581ba0e49de656a101bb4bf621586
A5TRATTO TEAM <a5trattoteam@gmail.com>
2021-05-10 21:49:31 +0200

    V.7.3.5.0 fix bug policy

--- template-policy.php
+++ template-policy.php
@@ -5,5 +5,5 @@ Template Name: Template Page Policy
 */
 ?>
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_part('templates/page-policy'); ?>
+    <?php get_part('templates/policy'); ?>
 <?php endwhile; ?>
