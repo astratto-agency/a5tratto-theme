30ae827e6f479c15a3bc374487e95dbe6c168d87
A5T <dev@a5tratto.it>
2021-10-04 16:57:18 +0200

    V.7.3.9.1 fix get_part('templates/page-'.$post->post_name); ?>

--- page.php
+++ page.php
@@ -4,4 +4,13 @@
 <?php endwhile; ?>
 <?php
 global $post;
-get_part('templates/page-' . $post->post_name); ?>
+// echo get_template_directory_uri().'/twig/page-'. $post->post_name.'.php';
+
+if (file_exists(get_template_directory_uri() . '/twig/page-' . $post->post_name . '.php')) {
+    echo 'temaplate slug page';
+    get_part('templates/page-' . $post->post_name);
+} else {
+    echo 'temaplate page';
+    get_part('templates/page');
+}
+?>
