fa193fdfe43c35406f983a461e0c393092227dbf
a5tratto-agency <dev@astratto.agency>
2023-03-14 15:26:46 +0100

    V.7.4.1.2 - fix 404

--- index.php
+++ index.php
@@ -7,16 +7,4 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_part('templates/content', get_post_format()); ?>
-<?php endwhile; ?>
-
-<?php if ($wp_query->max_num_pages > 1) : ?>
-  <nav class="post-nav">
-    <ul class="pager">
-      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'a5t')); ?></li>
-      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'a5t')); ?></li>
-    </ul>
-  </nav>
-<?php endif; ?>
-
+<?php get_part('templates/archive', get_post_format()); ?>
